ce6638a05e51b5fea5f4ed910c131fc968974208
Jeyi Adole <erthnice@gmail.com>
2021-12-10 20:27:28 +0100

    Dealing with children

--- src/index.js
+++ src/index.js
@@ -4,43 +4,42 @@ import ReactDom from 'react-dom';
 
 // CSS
 import './index.css';
-const firstBook = {
+const books = [
+{
+  author: "Unabriged",
+  img: "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg",
+  title: "Will"
+},
+{
   author: "Unabriged",
   img: "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg",
   title: "Will"
 }
-
-const author = 'Unabriged';
-const title = 'Will';
-const img = "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg";
+];
 
 function BookList () {
   return ( 
     <section className="bookList"> 
-  {/* This method shows repitition */}
-      < Book 
-      img={firstBook.img}
-      title={firstBook.title}
-      author={firstBook.author}
-      >
-        {/* children */}
-      <p>lorem20</p>
-      </Book>
+    {books.map((book) => {
+      const {img, title, author} = book;
 
- 
+      return (
+        <Book book = {book}></Book>
+      );
+    })}
     </section>
   );
-}
+}  
 
-// children name is special
-const Book = ({img, title, author, children}) => {
-  // const {img, title, author} = props;
+const Book = (props) => {
+  const {img, title, author} = props.book;
+  console.log(props)
   return (
     <article className='book'> 
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4> {author}</h4>
-      {children}
+
     </article>
   );
 };
