46ceb1d6495cee475a76670674fdefc61a96751b
Jeyi Adole <erthnice@gmail.com>
2021-12-11 21:03:06 +0100

    Different files combined

--- src/index.js
+++ src/index.js
@@ -4,20 +4,11 @@ import ReactDom from 'react-dom';
 
 // CSS
 import './index.css';
-const books = [
-{
-  id: 1,
-  author: "Unabriged",
-  img: "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg",
-  title: "Will"
-},
-{
-   id: 2,
-  author: "Unabriged",
-  img: "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg",
-  title: "Will"
-}
-];
+
+// JS Files
+// Don't need js extensions 
+import {books} from './books';
+import {Book} from './Book' 
 
 function BookList () {
   return ( 
@@ -31,30 +22,7 @@ function BookList () {
   );
 }  
 
-const Book = (props) => {
-  // Events in React
-  // attribute, eventHandler
-  // onClick, onMouseOver
-  const {img, title, author} = props;
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target)
-    alert('hello world')
-  };
-  const complexExample = (author) => {
-    console.log({author})
-  }
 
-  return (
-    <article className='book'> 
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4> {author}</h4>
-      <button type="button" onClick={clickHandler}>Reference</button>
-      <button type="button" onClick={() => complexExample(author)}>More complex example</button>
-    </article>
-  );
-};
 
 
 ReactDom.render(<BookList />, document.getElementById('root'));
