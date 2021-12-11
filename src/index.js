43adda8dde1a1c6c205f812c811bb36baae83ae6
Jeyi Adole <erthnice@gmail.com>
2021-12-11 20:47:27 +0100

    Changes for today

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import ReactDom from 'react-dom';
 import './index.css';
 const books = [
 {
+  id: 1,
   author: "Unabriged",
   img: "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg",
   title: "Will"
 },
 {
+   id: 2,
   author: "Unabriged",
   img: "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg",
   title: "Will"
@@ -21,10 +23,8 @@ function BookList () {
   return ( 
     <section className="bookList"> 
     {books.map((book) => {
-      const {img, title, author} = book;
-
-      return (
-        <Book book = {book}></Book>
+       return (
+        <Book key={book.id} {...book}></Book>
       );
     })}
     </section>
@@ -32,14 +32,26 @@ function BookList () {
 }  
 
 const Book = (props) => {
-  const {img, title, author} = props.book;
-  console.log(props)
+  // Events in React
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const {img, title, author} = props;
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target)
+    alert('hello world')
+  };
+  const complexExample = (author) => {
+    console.log({author})
+  }
+
   return (
     <article className='book'> 
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4> {author}</h4>
-
+      <button type="button" onClick={clickHandler}>Reference</button>
+      <button type="button" onClick={() => complexExample(author)}>More complex example</button>
     </article>
   );
 };
