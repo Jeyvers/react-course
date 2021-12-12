393cc400c19bd7c84372c54bc1e926dd6a3924b1
Jeyi Adole <erthnice@gmail.com>
2021-12-12 18:59:09 +0100

    useState

--- react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
-
+// useState - default react function 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // when useState is invoked, you pass in default values
+  // console.log(useState('hello world'))
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  // useState handles a value and a function handler
+
+  // common naming convention is to add set and the variable name as your function name
+  const [text, setText] = useState('random title');
+  const handleClick = (e) => {
+    if(text === 'random title') {
+      setText('hello world');
+    } else {
+      setText('random title')
+    }
+  }
+  return  ( 
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>Change Title</button>
+    </React.Fragment>
+
+  )
 };
 
 export default UseStateBasics;
--- react-advanced/src/tutorial/1-useState/setup/3-useState-array.js
+++ react-advanced/src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,29 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  const removeItem = (id) => {
+      let newPeople = people.filter(person => person.id !== id);
+      setPeople(newPeople);
+  }
+  return (
+  <>
+  <h2>useState array example</h2>;
+  {people.map((person) => {
+    const {id, name} = person;
+    return (
+      <div key={id} className="item">
+        <h4>{name}</h4>
+        <button onClick={() => removeItem(id)}>Remove</button>
+      </div>
+    );
+  })}
+  {/* funtion will run immediately on render without the arrow function bypass */}
+  <button className="btn" onClick={() => setPeople([])}>
+    clear items
+  </button>
+  </>
+  )
 };
 
 export default UseStateArray;
