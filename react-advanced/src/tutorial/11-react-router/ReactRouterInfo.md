c39c2333e06a0b5addaff284c2b70d4ba05e99f6
Jeyi Adole <erthnice@gmail.com>
2022-01-04 16:06:05 +0100

    React Router

--- react-advanced/src/tutorial/11-react-router/ReactRouterInfo.md
+++ react-advanced/src/tutorial/11-react-router/ReactRouterInfo.md
@@ -5,4 +5,8 @@ npm install react-router-dom
 
 ```
 
+<!-- Used in tutorial -->
+
+npm i react-router-dom@5.2.0
+
 "react-router-dom": "^5.2.0"
--- react-advanced/src/tutorial/11-react-router/setup/index.js
+++ react-advanced/src/tutorial/11-react-router/setup/index.js
@@ -10,7 +10,28 @@ import Person from './Person';
 // navbar
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return (
+    <Router>
+      {/* Exact is added so two or more routes won't be displayed at the same time. Only the exact match of the url will be displayed */}
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/people'>
+          <People />
+        </Route>
+        {/* url parameters path="/person/:id"*/}
+        <Route path='/:id' children={<Person />}></Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default ReactRouterSetup;
