15c431d5fa9ff32328e19ddad4419c3338ffef24
Jeyi Adole <erthnice@gmail.com>
2022-03-22 12:55:25 +0100

    Understanding props and destructuring

--- react-advanced-patterns/src/App.js
+++ react-advanced-patterns/src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const Button = (props) => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div
+      style={{ margin: '2rem auto' }}
+      onClick={() => setCounter((c) => c + props.increment)}
+    >
+      I am a button.
+      <div>
+        <button
+          style={{
+            margin: '1rem auto',
+            background: 'none',
+            color: props.color,
+            padding: '0.75rem 2rem',
+            borderRadius: '.5rem',
+            textDecoration: props.underline ? 'underline' : undefined,
+            fontSize: '1.5rem',
+          }}
+        >
+          Count {counter}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  const props = {
+    increment: 2,
+    underline: true,
+  };
+
+  // const obj = {
+  //   x: 1,
+  // };
+  // const obj2 = {
+  //   x: 2,
+  //   ...obj,
+  // };
+
+  // obj2.x === 1
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button {...props} color='black' />
+      <Button {...props} color='blue' />
+      <Button {...props} color='green' />
     </div>
   );
 }
